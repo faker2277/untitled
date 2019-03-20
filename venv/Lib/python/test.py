323fb16d73e9bba6e789b558ad7704aca7354cbc
faker2277 <owen2277@163.com>
2019-03-20 11:06:42 +0800

    aaaaa

--- venv/Lib/python/test.py
+++ venv/Lib/python/test.py
@@ -1,2 +1,20 @@
-a = [1,2,3,4,5]
-print(a)
+class Student():
+    pass
+paerl = Student()
+
+class PythonStudent():
+    name = None
+    age = 18
+    address = "china"
+    no = "001"
+
+    def dohomework(self):
+        print("做作业")
+        return None
+
+jack = PythonStudent()
+
+print(jack.address)
+print(jack.age)
+print(jack.dohomework())
+
