415a835494c401d197fb0e6ff7a99306b0d9e91f
faker2277 <owen2277@163.com>
2019-03-28 11:14:21 +0800

    aaaaa

--- venv/Lib/python/def_test.py
+++ venv/Lib/python/def_test.py
@@ -1,36 +1,47 @@
-#导入模块部分
-import time
+class Student():
+    # def __init__(self, name):
+    #     self.name = name
+    #
+    # def __gt__(self, obj):
+    #     print("哈哈，A 的值{0} 会比B 的值{1}大么！".format(self, obj))
+    #     return self.name < obj.name
+    # def __str__(self):
+    #     return self.name
 
-
-#定义类部分
-class Python():
-    name = "LiLi"
-    age = 18
-
-    #定义函数
-    def say(self):
-        self.name = "Anlge"
-        self.age = 21
-        print("The name is : {0}".format(Python.name))
-        print("The name is : {0}".format(Python.age))
-
-class Name_t():
-    name = "原有"
-    _age = 20
-
-class pyther():
+# p1 = Student("one")
+# p2 = Student("two")
+# print(p1 > p2)
+#print(Student.__str__)
     '''
-    拉的计费啊啊啊
+    这是一条注释
     '''
+    name = 'ok'
+    age = 16
+    # def __getattr__(self, name):
+    #     print("您所定义的属性未正确，请调整！谢谢！")
+    def __init__(self):
+        self.name = "sss"
+        self.age = 11
+    def fget(self):
+        return self._name + '-卫浴'
 
+    def fset(self,name):
+        self._name = name.upper()
 
-print(issubclass(B, A))
-print(issubclass(C, A))
-print(issubclass(C, object))
+    def fdel(self):
+        self._name = "No Name"
+    def doc(self):
+        pass
 
-Hello = Name_t()
+    name = property(fget, fset, fdel, "Is OK")
+a = Student()
+print(a.name)
+# print(a.add)
 
-print(Hello.name)
-print(Hello._age)
-# print(pyther.__doc__)
-
+# p1 = Student()
+# p1.name = "toto-"
+# print(p1.name)
+# print(Student.__dict__)
+# print(Student.__name__)
+# print(Student.__doc__)
+# print(Student.__bases__)
--- venv/Lib/python/node.md
+++ venv/Lib/python/node.md
@@ -2,3 +2,30 @@
 - 提示符下：conda create -n oop python=3.6
 
 - print(a.__mro__)  用来显示当前层级关系，如显示A类的父类及最顶父类
+
+- property: 属性修饰符，可带参数（tget,tset,tdel,doc）
+# 类的常用魔法方法
+
+- 操作类
+    - 返回一个类的属性
+    print(Student.__dict__)
+    - 返回一个类的名称
+    print(Student.__name__)
+    - 返回一个类的注释
+    print(Student.__doc__)
+    - 返回一个类的父类
+    print(Student.__bases__)
+    - 返回一个调用的方法，对象当做函数的时候触发
+    print(Student.__call__)
+    - 返回一个字符串，当对象被当做字符串使用的时候调用
+    print(Student.__str__) || print(Student.__repr__) 类似
+- 描述符
+    - '__set__'
+    - '__get__'
+    - '__delete__'
+- 属性操作相关
+    - '__getattr__' :访问一个不存在的属性时触发
+    
+
+
+
